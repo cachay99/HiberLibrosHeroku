ee39e8c961a7d1916dedc69f46fd656b4462778a
Jesus <Nombre>
2021-10-04 13:27:41 +0200

    Hola

--- src/main/java/com/hiberlibros/HiberLibros/controllers/UsuarioController.java
+++ src/main/java/com/hiberlibros/HiberLibros/controllers/UsuarioController.java
@@ -63,7 +63,7 @@ public class UsuarioController {
 
     @PostMapping("/editarUsuario")//edita usuario, manda el usuario para rellenar el formulario
     public String usuarioEditar(Usuario u) {
-        return "redirect:hiberlibros/panelUsuario";
+        return "redirect:/hiberlibros/panelUsuario";
     }
 
     @GetMapping("/borrar")
